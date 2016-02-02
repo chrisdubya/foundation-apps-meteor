a49a1d64dfeb2e5868308240634ee447f4c9b4e2
Chris Williams <chris@niceandserious.com>
2016-02-02 16:35:32 +0000

    bump to 1.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'chrisdubya:foundation-apps',
     summary: 'Foundation Apps Wrapper for Meteor.',
-    version: '1.0.1',
+    version: '1.0.2',
     git: 'https://github.com/chrisdubya/foundation-apps-meteor',
     documentation: 'meteor-README.md'
 });
