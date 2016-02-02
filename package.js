bc52e9eb15cbbac50cbb389509ae6e1b582c473c
Chris Williams <chris@niceandserious.com>
2016-02-02 12:43:02 +0000

    updated version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'chrisdubya:foundation-apps',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Foundation Apps Meteor Package with SCSS and JS',
   // URL to the Git repository containing the source code for this package.
